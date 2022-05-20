696a93c7ae732cf3e91da4437a3f72508895422d
Jeet Limbachiya <73994026+JeetLimbachiya@users.noreply.github.com>
2022-05-20 14:21:46 +0530

    Update upload_file_to_S3.py

--- upload_file_to_S3.py
+++ upload_file_to_S3.py
@@ -1,24 +1,20 @@
+        
 import boto3
 s3 = boto3.client('s3')
-with open("Output.mp4","rb") as f:
-    s3.upload_fileobj(f, "fbwithoutbucket","Output.mp4")
+with open("Students_1.mp4","rb") as f:
+    s3.upload_fileobj(f, "faceredactionbucket","Students_1.mp4")
 
-url = boto3.client('s3').generate_presigned_url(
+
+from boto3 import client
+from botocore.client import Config
+
+s3 = client('s3', region_name="us-east-2", config=Config(signature_version='s3v4'))
+url = s3.generate_presigned_url(
     ClientMethod='get_object',
-    Params={'Bucket': 'faceredactionbucket', 'Key': 'ezgif.com-gif-maker (1).gif'},
-    ExpiresIn=3600)
+        Params={'Bucket': 'faceredactionbucket', 'Key': 'Students_1.mp4'},
+        ExpiresIn=3600,
+    )
 
-print("This is URL:",url)
+print(url)
 
 
-#class Upload:
-
-#    def __init__(self, s3bucket, output):
-#        self.s3bucket = s3bucket
-#        self.output = output
-
-#        self.s3 = boto3.client('s3')
-
-#    if __name__=='__main__':
-#        with open(self.output, "rb") as f:
-#          self.s3.upload_fileobj(f, self.s3bucket, self.output)
