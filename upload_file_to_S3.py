e8537e9270a9199041ebb389e417c273c21639fc
Jeet Limbachiya <73994026+JeetLimbachiya@users.noreply.github.com>
2022-06-23 15:47:12 +0530

    Update upload_file_to_S3.py

--- upload_file_to_S3.py
+++ upload_file_to_S3.py
@@ -29,13 +29,12 @@ print(url)
 #response = sns.publish(TopicArn='arn:aws:sns:us-east-2:673304395320:SES_EC2',Message= msg + url)
 #print(response)
 
-#======================================
-
+#========================This is working===============================
 ses = boto3.client('ses',region_name="us-east-2")#,config=Config(X-Amz-Algorithm==AWS4-HMAC-SHA256,X-AMZ-Expires==300))
 response = ses.send_email(
     Source='jlimbachiya@safetyvision.com',
     Destination={
-        'ToAddresses': ['jeet.limbachiya@relayhumancloud.com','jeet@svcloud.live','afulzele@safetyvision.com']
+        'ToAddresses': ['jeet.limbachiya@relayhumancloud.com','jeet@svcloud.live']
     },
     Message={
         'Subject': {
@@ -53,6 +52,102 @@ response = ses.send_email(
 })
 
 print(response)
+
+#=========================This is also working===============================
+
+import os
+import boto3
+from botocore.exceptions import ClientError
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+
+# Setup the basics here
+SENDER = "JL <jeet@svcloud.live>"
+#RECIPIENT = "jeet.limbachiya@relayhumancloud.com"
+SUBJECT = "Sending a URL of S3 object alongwith video file"
+
+# The configuration set used to track mails
+#CONFIGURATION_SET = "ConfigSet"
+
+# The full path to the file that will be attached to the email.
+ATTACHMENT = "/home/ubuntu/AWS-FaceBlurring/AWS_FB/Output.mp4"
+
+# The email body for recipients with non-HTML email clients.
+BODY_TEXT = "Hello,\r\nBelow is a URL of S3 object of f'date: {d}'."
+
+# The HTML body of the email.
+BODY_HTML =f"Below is a URL of S3 object of date: {d}\n: " + url #"""\
+#<html>
+#<head></head>
+#<body>
+#<h1>Hello!</h1>
+#<p>Below is a URL of S3 object of f"date: {d}"</p>
+#<a href=url>Cick here</arrr>
+#</body>
+#</html>
+#"""
+
+# The character encoding for the email.
+CHARSET = "utf-8"
+
+# Create a new SES resource and specify a region.
+client = boto3.client('ses')
+
+# Create an instance of multipart/mixed parent container.
+msg = MIMEMultipart('mixed')
+
+# Add subject, from and to lines.
+msg['Subject'] = SUBJECT
+msg['From'] = SENDER
+#msg['To'] = RECIPIENT
+
+# Create a multipart/alternative child container.
+msg_body = MIMEMultipart('alternative')
+
+# Encode the text and HTML content and set the character encoding. This step is
+# necessary if you're sending a message with characters outside the ASCII range.
+textpart = MIMEText(BODY_TEXT.encode(CHARSET), 'plain', CHARSET)
+htmlpart = MIMEText(BODY_HTML.encode(CHARSET), 'html', CHARSET)
+
+# Add the text and HTML parts to the child container.
+msg_body.attach(textpart)
+msg_body.attach(htmlpart)
+
+# Define the attachment part and encode it using MIMEApplication.
+att = MIMEApplication(open(ATTACHMENT, 'rb').read())
+# Add a header to tell the email client to treat this part as an attachment,
+# and to give the attachment a name.
+att.add_header('Content-Disposition','attachment',filename=os.path.basename(ATTACHMENT))
+
+# Attach the multipart/alternative child container to the multipart/mixed
+# parent container.
+msg.attach(msg_body)
+
+# Add the attachment to the parent container.
+msg.attach(att)
+#print(msg)
+try:
+    #Provide the contents of the email.
+    response = client.send_raw_email(
+        Source=SENDER,
+        Destinations=[ jeet.limbachiya@relayhumancloud.com','jeet@svcloud.live','shrikant.viswanathan@relayhumancloud.com'
+
+            #RECIPIENT
+        ],
+        RawMessage={
+            'Data':msg.as_string(),
+        },
+       # ConfigurationSetName=CONFIGURATION_SET
+    )
+# Display an error if something goes wrong.
+except ClientError as e:
+    print(e.response['Error']['Message'])
+else:
+    print("Email sent! Message ID:"),
+    print(response['MessageId'])
+
+#=====================================================
 
 
 
